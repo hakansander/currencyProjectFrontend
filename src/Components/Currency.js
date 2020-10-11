88333d82434f1567e0ef8d6defc87331cb60c564
Hakan Sander <hakansander@gmail.com>
2020-10-11 13:54:12 +0300

    Add currency listing

--- src/Components/Currency.js
+++ src/Components/Currency.js
@@ -5,24 +5,38 @@ class Currency extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            contacts: [],
+            contacts: []
         };
     }
 
     componentDidMount() {
-        fetch('http://localhost:8080/currency/TRY')
+        fetch('http://localhost:8080/currency/USD')
             .then(response => response.json())
             .then(data => this.setState({contacts: data}))
     }
 
     render() {
-        console.log(this.state.contacts)
+        var arr = [];
+        let rates = this.state.contacts["rates"];
+
+        if(typeof rates != "undefined") {
+            {Object.entries(rates).map(entry => (
+                arr.push(entry)
+            ))}
+        }
+
+        arr.sort(function (a, b) {
+            return a[0].localeCompare(b[0]);
+        });
+
         return (
             <div>
                 <div className="row">
-                    { this.state.contacts.map((item) => (
-                        <SingleCurrency key={item.id} item={item} />
-                    ))}
+                    <ul>
+                        {arr.map(item => (
+                            <SingleCurrency key={item[0]} item={item} />
+                        ))}
+                    </ul>
                 </div>
             </div>
         )
--- src/Components/SingleCurrency.js
+++ src/Components/SingleCurrency.js
@@ -1,18 +1,7 @@
 import React from 'react';
 
 const SingleCurrency = ({item}) => (
-    <div className="row">
-        <div className="col s12 m6">
-            <div className="card blue-grey darken-1">
-                <div className="card-content white-text">
-                    <span className="card-title">{item.date}</span>
-                </div>
-                <div className="card-action">
-                    <p>{item.email}</p>
-                </div>
-            </div>
-        </div>
-    </div>
+    <li>{item[0]} {item[1]}</li>
 );
 
 export default SingleCurrency;
